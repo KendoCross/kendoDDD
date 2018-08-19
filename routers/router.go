316a0a0ace096e73fd66df057f4d7cbdc44ad179
KendoCross <ChenyWork@hotmail.com>
2018-08-19 20:47:26 +0800

    修改

--- routers/router.go
+++ routers/router.go
@@ -5,18 +5,8 @@ import (
 	"github.com/astaxie/beego"
 )
 
-type MainController struct {
-	beego.Controller
-}
-
-func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
-}
-
 func init() {
-	beego.Router("/", &MainController{})
+	beego.Router("/", &presentation.MainController{})
 	beego.Router("/Protocol/Sign", &presentation.ProtocolController{}, "Post:Sign")
 	beego.Router("/Protocol/SignCfrm", &presentation.ProtocolController{}, "*:SignCfrm")
 }
